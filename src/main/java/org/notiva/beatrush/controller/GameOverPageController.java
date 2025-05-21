82bdcc294d584290b91974da35b959ff112f9a93
AndyXue1019 <a931019ing@gmail.com>
2025-05-21 23:58:53 +0800

    UPDATE: path issue in GameOverPageController

--- src/main/java/org/notiva/beatrush/controller/GameOverPageController.java
+++ src/main/java/org/notiva/beatrush/controller/GameOverPageController.java
@@ -5,14 +5,13 @@ import javafx.event.ActionEvent;
 import org.notiva.beatrush.core.StageManager;
 
 public class GameOverPageController {
-    public void onContinueButtonClick(ActionEvent actionEvent) {
+    public void onContinueButtonClick(ActionEvent ignoredActionEvent) {
         StageManager stageManager = StageManager.getInstance();
-        stageManager.showStage("BeatRush", "SongSelectPage.fxml");
-
+        stageManager.showStage("BeatRush", "/view/page/SongSelectPage.fxml");
     }
 
-    public void onReplayButtonClick(ActionEvent actionEvent) {
+    public void onReplayButtonClick(ActionEvent ignoredActionEvent) {
         StageManager stageManager = StageManager.getInstance();
-        stageManager.showStage("BeatRush", "RhythmGamePage.fxml");
+        stageManager.showStage("BeatRush", "/view/page/RhythmGamePage.fxml");
     }
 }
