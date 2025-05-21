95e0864c45920788be13664fee55af4399951337
AndyXue1019 <a931019ing@gmail.com>
2025-05-21 23:49:32 +0800

    UPDATE: all.css, GameOverPage, ScorePage

--- src/main/java/org/notiva/beatrush/controller/GameOverPageController.java
+++ src/main/java/org/notiva/beatrush/controller/GameOverPageController.java
@@ -1,4 +1,18 @@
 package org.notiva.beatrush.controller;
 
+import javafx.event.ActionEvent;
+
+import org.notiva.beatrush.core.StageManager;
+
 public class GameOverPageController {
+    public void onContinueButtonClick(ActionEvent actionEvent) {
+        StageManager stageManager = StageManager.getInstance();
+        stageManager.showStage("BeatRush", "SongSelectPage.fxml");
+
+    }
+
+    public void onReplayButtonClick(ActionEvent actionEvent) {
+        StageManager stageManager = StageManager.getInstance();
+        stageManager.showStage("BeatRush", "RhythmGamePage.fxml");
+    }
 }
