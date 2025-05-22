f15502bae8a9661f3458d770eb81edbe74a55f9e
RogelioKG <rogeliokg0813@gmail.com>
2025-05-22 19:49:58 +0800

    UPDATE: StartMenuPage proto finished

--- src/main/java/org/notiva/beatrush/controller/StartMenuPageController.java
+++ src/main/java/org/notiva/beatrush/controller/StartMenuPageController.java
@@ -1,11 +1,18 @@
 package org.notiva.beatrush.controller;
 
 import javafx.fxml.FXML;
+import org.notiva.beatrush.core.StageManager;
 
 
 public class StartMenuPageController {
 
     @FXML
     protected void initialize() {
+    }
+
+    @FXML
+    protected void onStartButtonClick() {
+        StageManager stageManager = StageManager.getInstance();
+        stageManager.showStage("BeatRush", "/view/page/SongSelectPage.fxml");
     }
 }
