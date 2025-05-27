4fcd6af9e5977ebe7da74f4364f0014bf2eef5a8
RogelioKG <rogeliokg0813@gmail.com>
2025-05-28 05:58:40 +0800

    CLEAN

--- src/main/java/org/notiva/beatrush/App.java
+++ src/main/java/org/notiva/beatrush/App.java
@@ -2,18 +2,11 @@ package org.notiva.beatrush;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.notiva.beatrush.core.MediaManager;
-import org.notiva.beatrush.core.StageManager;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        MediaManager mediaManager = MediaManager.getInstance();
-        mediaManager.loadAll();
-        StageManager stageManager = StageManager.getInstance();
-        stageManager.registerStage("BeatRush", primaryStage);
-        stageManager.showStage("BeatRush", "/view/page/StartMenuPage.fxml");
     }
 
     public static void main(String[] args) {
